ae36f59e8e21526137a94eaeb8e86cdcc6c0f36c
Sarsen <blazer.fei@gmail.com>
2025-06-16 00:23:21 +0900

    Header

---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  spaceBetween: 100,
+	  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true
+});
